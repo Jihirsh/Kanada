1895f4ae22f29004c822f1b43946d56279c49ee1
Jihirsh <jihirshsingh12@gmail.com>
2025-08-01 21:27:55 +0530

    redirect done

---
+++ app/(routes)/search/[libId]/_components/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Clock, Link, Send } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import moment from "moment";
+
+function Header({ searchInputRecord }) {
+  return (
+    <div className="p-4 border-b flex justify-between items-center">
+      <div className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex gap-1 items-center">
+          <Clock className="h-5 w-5 text-gray-500" />
+          <h2 className="text-sm text-gray-500">
+            {moment(searchInputRecord?.created_at).fromNow()}
+          </h2>
+        </div>
+      </div>
+
+      <h2 className="line-clamp-1">{searchInputRecord?.searchInput}</h2>
+
+      <div className="flex gap-3">
+        <Button>
+          <Link />
+        </Button>
+        <Button>
+          <Send />
+          Share
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- app/(routes)/search/[libId]/page.jsx
+++ app/(routes)/search/[libId]/page.jsx
@@ -1,12 +1,28 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import Header from './_components/Header';
+import DisplayResult from './_components/DisplayResult';
 
 function SearchQueryResult() {
   const { libId } = useParams();
-  console.log(libId);
+  const [searchInputRecord, setSearchInputRecord] = useState();
+
+  useEffect(() => {
+    GetSearchQueryRecord();
+  }, [])
+
+  const GetSearchQueryRecord = () => {
+    
+  }
+  
   return (
-    <div>SearchQueryResult</div>
+    <div className="w-full">
+        <Header searchInputRecord={searchInputRecord} />
+        <div className="px-10 md:px-20 lg:px-36 xl:px-56 mt-20">
+            <DisplayResult searchInputRecord={searchInputRecord} />
+        </div>
+    </div>
   )
 }
 
